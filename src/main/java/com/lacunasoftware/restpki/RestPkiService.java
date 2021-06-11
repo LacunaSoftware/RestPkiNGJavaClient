b4a0ea9932e701709266c318379de1971b7fe79b
EmilyS <emily.souza.rs@gmail.com>
2021-06-11 16:32:09 -0300

    [RPNG-90] Added new methods to RestPkiService interface

--- src/main/java/com/lacunasoftware/restpki/RestPkiService.java
+++ src/main/java/com/lacunasoftware/restpki/RestPkiService.java
@@ -1,9 +1,13 @@
 package com.lacunasoftware.restpki;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javafx.util.Pair;
 import java.util.UUID;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.OffsetDateTime;
 
 /**
  * RestPkiService
@@ -19,7 +23,7 @@ public interface RestPkiService {
     Document getDocument(UUID id) throws RestException;
 
     Document getDocument(DocumentModel model);
-    
+
     public List<Signer> getDocumentSigners(UUID id) throws RestException;
 
     public  Document findDocumentByKey(String key) throws RestException, IOException;
@@ -29,4 +33,22 @@ public interface RestPkiService {
     byte[] getContent(String location) throws RestException, IOException;
 
     DocumentModel GetDocumentModel(UUID id) throws RestException;
+
+    DocumentKeyModel AllocateDocumentKey(Map<String, List<String>> provisionalMetadata, UUID subscriptionId) throws RestException;
+
+    List<DocumentKeyModel> AllocateDocumentKeys(int count, Map<String, List<String>> provisionalMetadata, UUID subscriptionId) throws RestException;
+
+    // region Application management
+
+    ApplicationModel CreateApplication(String name, List<Roles> roles,  Map<String, List<String>> defaultDocumentMetadata, UUID subscriptionId) throws RestException;
+
+    CreateApplicationApiKeyResponse CreateApplicationKey(UUID applicationId, OffsetDateTime expiresOn, String description) throws RestException;
+
+    Pair<ApplicationModel, String> CreateApplicationAndKey(String name, List<Roles> roles,  Map<String, List<String>> defaultDocumentMetadata, UUID subscriptionId) throws RestException;
+
+    Map<String, List<String>> GetApplicationDefaultDocumentMetadata(UUID applicationId) throws RestException;
+
+    Map<String, List<String>> UpdateApplicationDefaultDocumentMetadata(UUID applicationId,  Map<String, List<String>> defaultDocumentMetadata) throws RestException;
+
+    // endregion
 }
