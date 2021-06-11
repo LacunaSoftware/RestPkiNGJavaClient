d7e42fb7b7f109c010d47e1c5f63911a7b672b70
EmilyS <emily.souza.rs@gmail.com>
2021-06-11 16:35:02 -0300

    [RPNG-90] Edited autogenerated file to fix build

--- src/main/java/com/lacunasoftware/restpki/CreateApplicationApiKeyRequest.java
+++ src/main/java/com/lacunasoftware/restpki/CreateApplicationApiKeyRequest.java
@@ -17,7 +17,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.threeten.bp.OffsetDateTime;
+import java.time.OffsetDateTime;
 
 /**
  * CreateApplicationApiKeyRequest
